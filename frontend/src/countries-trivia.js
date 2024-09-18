7c3a8a9b5fe8bdcf4c25fb78550b5570bef7270e
rachel <rachel.koh@vanderbilt.edu>
2024-09-18 01:31:44 -0500

    Commit frontend

---
+++ frontend/src/countries-trivia.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+
+function CountriesTrivia() {
+  const [input, setInput] = useState("");
+  const [capital, setCapital] = useState("");
+  // const [country, setCountry] = useState('');
+
+  // Function to fetch the capital from the backend
+  const fetchCapital = () => {
+    // Send a GET request to the backend
+    fetch("http://localhost:3000/capital")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data: ", data);
+        setCapital(data.capital || "Error fetching capital"); // Set the capital from the backend response
+        // setCountry(data.name || 'Error fetching country'); // Set the country from the backend response
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setCapital("Error fetching country details");
+      });
+  };
+
+  // Fetch the capital when the component mounts
+  useEffect(() => {
+    fetchCapital();
+  }, []); // Empty dependency array to only run once
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the page from refreshing
+    console.log("in verification func");
+
+    // Make a POST request to the backend
+    fetch("http://localhost:3000/validate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input: input }), // Send the input string as part of the request body
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Truth value of guess: ", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Guess the Country!</h1>
+
+      <p>Can you guess the country based on its capital city?</p>
+
+      {/* Display the capital */}
+      <p>Capital: {capital}</p>
+
+      {/* Form to input the string literal */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            // console.log(input);
+            // console.log(e.target.value);
+            setInput(e.target.value);
+          }}
+          placeholder="Guess the country!"
+        />
+        <button type="submit">Guess</button>
+      </form>
+      <br></br>
+
+      {/* Button to fetch a new capital */}
+      <button onClick={fetchCapital}>Get a New Capital</button>
+    </div>
+  );
+}
+
+export default CountriesTrivia;
